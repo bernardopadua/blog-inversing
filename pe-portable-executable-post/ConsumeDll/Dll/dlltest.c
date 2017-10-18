871e579dd07c4d9ef470cc0ec5a8f8028a0725e5
Bernardo de Pádua Oliveira <bernardopadua@users.noreply.github.com>
2017-10-18 18:28:30 -0200

    Update dlltest.c
    Change language ;P

--- pe-portable-executable-post/ConsumeDll/Dll/dlltest.c
+++ pe-portable-executable-post/ConsumeDll/Dll/dlltest.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <windows.h>
 
-__declspec(dllexport) void NONAME (const char *nome) {
-    printf("Olá, %s", nome);
+__declspec(dllexport) void NONAME (const char *name) {
+    printf("Hello, %s", name);
 }
